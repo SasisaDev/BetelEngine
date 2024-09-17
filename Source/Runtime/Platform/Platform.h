c2643fcd12fadd7cdb051724777f4d72efa9c756
きゐ <40644016+SasisaDev@users.noreply.github.com>
2024-09-17 23:48:30 +0300

    GCC __stdcall fix

--- Source/Runtime/Platform/Platform.h
+++ Source/Runtime/Platform/Platform.h
@@ -5,11 +5,12 @@
 #include "Interface/IPlatform.h"
 #include "Interface/Files/IFile.h"
 
+#ifndef PLATFORM_WINDOWS
+#   define __stdcall
+#endif
+
 #ifdef PLATFORM_WINDOWS
 #	include "Windows/WindowsPlatform.h"
 #elif defined(PLATFORM_LINUX)
-#if defined(__GNUC__)
-#	define __stdcall
-#endif
 #	include "Linux/LinuxPlatform.h"
 #endif
