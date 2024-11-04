56a6c586692163e1596fb1a0eaa1b5a53b505d95
きゐ <40644016+SasisaDev@users.noreply.github.com>
2024-11-04 20:26:05 +0300

    Adjusted imgui vertex shader for srgb
    pow(component, 2.2f)

--- Source/Editor/EditorUI/EditorToolkit.h
+++ Source/Editor/EditorUI/EditorToolkit.h
@@ -8,13 +8,63 @@ class EditorToolkitBase : public EditorToolkit {
 public:
 
     virtual void OnGUI() override {
+        if (ImGui::BeginMainMenuBar()) {
+            if (ImGui::BeginMenu("File")) {
+                if (ImGui::MenuItem("Create")) { 
+                }
+                if (ImGui::MenuItem("Open", "Ctrl+O")) { 
+                }
+                if (ImGui::MenuItem("Save", "Ctrl+S")) {
+                }
+                if (ImGui::MenuItem("Save as..")) { 
+                }
+                ImGui::EndMenu();
+            }
+            if (ImGui::BeginMenu("Window")) {
+                if (ImGui::MenuItem("Viewport")) { 
+                }
+                if (ImGui::MenuItem("Asset Explorer")) { 
+                }
+                if (ImGui::MenuItem("World Outliner")) { 
+                }
+                if (ImGui::MenuItem("Details")) { 
+                }
+                ImGui::EndMenu();
+            }
+            ImGui::EndMainMenuBar();
+        }
+
+        ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->WorkPos);
+        ImGui::SetNextWindowSize(viewport->WorkSize);
+        ImGui::SetNextWindowViewport(viewport->ID);
+
+        ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
+        ImGuiWindowFlags host_window_flags = 0;
+        host_window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoDocking;
+        host_window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+        host_window_flags |= ImGuiWindowFlags_NoBackground;
+
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("DockSpace Window", nullptr, host_window_flags);
+        ImGui::PopStyleVar(3);
+
+        ImGuiID dockspace_id = ImGui::GetID("DockSpace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags, nullptr);
+        ImGui::End();
+
+        ImGui::SetNextWindowBgAlpha(0.75f);
         ImGui::SetNextWindowSize(ImVec2(350, 200), ImGuiCond_Once);
-        if(ImGui::Begin("Test Window")){
+        ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.0f, 0.0f, 0.0f, 0.75f));
+        if(ImGui::Begin("Test Window", 0, ImGuiWindowFlags_NoCollapse)){
             ImGui::Text("Some random text");
             if(ImGui::Button("Random Function")) {
                 LOG(Log, LogTest, "ImGui button pressed");
             }
         }
         ImGui::End();
+        ImGui::PopStyleColor();
     }
 };
